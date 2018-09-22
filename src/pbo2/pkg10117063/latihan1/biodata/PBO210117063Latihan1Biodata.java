ae3a88ac4639c6bb4e47b41ff9eda163d787dad1
Cessario <43488314+Cessario@users.noreply.github.com>
2018-09-22 20:11:48 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117063/latihan1/biodata/PBO210117063Latihan1Biodata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan1.biodata;
+
+/**
+ *
+ * @author DEADPOOL
+ *NAMA : Cessario
+ *KELAS : IF-2
+ *NIM : 10117063
+ *Deskripsi Program : menampilkan biodata */
+public class PBO210117063Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA" + "\n" + "-----------------------------");
+        System.out.println("NAMA\t\t: Cessario Sheva L.P.A");
+        System.out.println("ALAMAT\t\t: Pasirkaliki, Cimahi Utara ");
+        System.out.println("Tempat Lahir\t: Bekasi");
+        System.out.println("Tanggal Lahir\t: 06 December 1999");
+        System.out.println("-----------------------------");
+        System.out.println("Hobi\t\t: Menggambar");
+        System.out.println("Makes\t\t: Pisang & Telur");
+        System.out.println("Mikes\t\t: Jus Pisang");
+    }
+    
+}
